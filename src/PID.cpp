a4191a2052c6ba8755bc735503c0bf0dabab181e
Mohammed Nasir <mnehaal@gmail.com>
2019-08-08 15:56:41 -0700

    added throttle PID

--- src/PID.cpp
+++ src/PID.cpp
@@ -15,11 +15,11 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
   Kp = Kp_;
   Ki = Ki_;
   Kd = Kd_;
-  
+
   p_error = 0.0;
   i_error = 0.0;
   d_error = 0.0;
-  
+
   prev_cte = 0.0;
   sum_cte = 0.0;
 
@@ -29,13 +29,13 @@ void PID::UpdateError(double cte) {
   /**
    * TODO: Update PID errors based on cte.
    */
-  	p_error = Kp * cte;
-  
-    sum_cte += cte;
-  	i_error = Ki * sum_cte;
-  
-    d_error = Kd * (cte - prev_cte);
-    prev_cte = cte;
+  p_error = Kp * cte;
+
+  sum_cte += cte;
+  i_error = Ki * sum_cte;
+
+  d_error = Kd * (cte - prev_cte);
+  prev_cte = cte;
 
 }
 
@@ -44,6 +44,6 @@ double PID::TotalError() {
    * TODO: Calculate and return the total error
    */
   double total_error = p_error + i_error + d_error;
-  
+
   return total_error;  // TODO: Add your total error calc here!
 }
--- src/main.cpp
+++ src/main.cpp
@@ -34,12 +34,14 @@ int main() {
   uWS::Hub h;
 
   PID pid;
+  PID pid_s;
   /**
    * TODO: Initialize the pid variable.
    */
-  pid.Init(0.1, 0.001, 2.8); //P,I,D
+  pid.Init(0.1, 0.001, 2.5); //P,I,D
+  pid_s.Init(1.0, 0.0, 0.1); //throttle
 
-  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
+  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -57,7 +59,8 @@ int main() {
           double cte = std::stod(j[1]["cte"].get<string>());
           double speed = std::stod(j[1]["speed"].get<string>());
           double angle = std::stod(j[1]["steering_angle"].get<string>());
-          double steer_value;
+          double speed_setpoint = 10.0;
+          double steer_value, throttle;
           /**
            * TODO: Calculate steering value here, remember the steering value is
            *   [-1, 1].
@@ -66,14 +69,17 @@ int main() {
            */
           pid.UpdateError(cte);
           steer_value = -pid.TotalError();
-          
+
+          pid_s.UpdateError(speed_setpoint-speed);
+          throttle = pid_s.TotalError();
+
           // DEBUG
-          std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
+          std::cout << "CTE: " << cte << " Steering Value: " << steer_value
                     << std::endl;
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.5;
+          msgJson["throttle"] = throttle;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
@@ -90,7 +96,7 @@ int main() {
     std::cout << "Connected!!!" << std::endl;
   });
 
-  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, 
+  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
                          char *message, size_t length) {
     ws.close();
     std::cout << "Disconnected" << std::endl;
@@ -103,6 +109,6 @@ int main() {
     std::cerr << "Failed to listen to port" << std::endl;
     return -1;
   }
-  
+
   h.run();
 }
